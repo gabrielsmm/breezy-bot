9388bbe4688005265198dac88cb2e701a63d6486
gabrielsmm <gabrielsmm251@gmail.com>
2022-12-21 21:33:39 -0300

    Correção bug ao baixar vídeo

--- commands/video.js
+++ commands/video.js
@@ -16,7 +16,15 @@ module.exports = {
         try {
             let link = interaction.options.getString('link');
             let usuario = interaction.user.username;
+
+            await interaction.deferReply({ ephemeral: false });
+
             const response = await axios.get(linkFileDownloader + link);
+
+            if (response.data.download_url === null || response.data.download_url === undefined) {
+                await interaction.editReply({ content: 'Não foi possível converter o vídeo', ephemeral: false });
+                return;
+            }
 
             const downloader = new Downloader({
                 url: response.data.download_url,
@@ -27,20 +35,29 @@ module.exports = {
                 },
             });
 
-            await interaction.deferReply();
-
-            const {filePath, downloadStatus} = await downloader.download();
-
-            await interaction.editReply('Baixado!');
+            try {
+                await downloader.download();
+            } catch (error) {
+                await interaction.editReply({ content: 'Erro ao baixar o vídeo.', ephemeral: false });
+                console.log(error);
+                return;
+            }
 
             await interaction.channel.send({
                 files: [`./videos/${usuario}.mp4`]
             });
 
+            await interaction.editReply({ content:'Baixado!', ephemeral: false });
+
             // limpando a pasta
             fs.emptyDirSync('./videos');
         } catch (error) {
-            await interaction.reply({ content: 'Ocorreu um erro ao tentar baixar a mídia', ephemeral: true });
+            if (error.code === 40005) {
+                interaction.editReply({ content: 'Ops, vídeo muito grande para ser enviado, desculpe.', ephemeral: false });
+            } else {
+                interaction.editReply({ content: 'Ocorreu um erro ao tentar baixar o vídeo.', ephemeral: false });
+            }
+            fs.emptyDirSync('./videos');
             console.log(error);
         }
 	},
