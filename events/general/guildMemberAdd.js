3aeea5de9b99e5531c21210b050ba08a2787fe17
gabrielsmm <gabrielsmm251@gmail.com>
2023-03-18 21:21:57 -0300

    Correção bug mensagem de boas vindas

--- events/general/guildMemberAdd.js
+++ events/general/guildMemberAdd.js
@@ -8,18 +8,19 @@ module.exports = {
 
         if(!member.guild) return;
 
-        const parameters = `username=${member.user.username}&usernameColor=FFF&discriminatorColor=FFF&title=Bem+vindo+ao+${member.guild.name}!&titleColor=C0C0C0&text=Vamos+jogar?&textColor=C0C0C0&borderColor=C0C0C0&background=https://img.freepik.com/vetores-gratis/forma-diagonal-geometrica-abstrata-em-fundo-escuro_1409-1819.jpg&image=${member.user.displayAvatarURL({ extension: 'jpg' })}`;
+        const parameters = `username=${member.user.username}&usernameColor=FFF&discriminatorColor=FFF&title=Bem+vindo+ao+${member.guild.name}!&titleColor=C0C0C0&text=%20&textColor=C0C0C0&borderColor=C0C0C0&background=https://img.freepik.com/vetores-gratis/forma-diagonal-geometrica-abstrata-em-fundo-escuro_1409-1819.jpg&image=${member.user.displayAvatarURL({ extension: 'jpg' })}`;
 
         let link = linkGenerator + parameters;
 
         const file = new AttachmentBuilder(link, { name: 'welcome.png' });
         const welcomeEmbed = new EmbedBuilder()
         .setColor('Purple')
-        .setDescription(`Opa ${member.user}, vamos jogar?`)
-        .setImage(`attachment://welcome.png`)
-        .setTimestamp();
+        .setDescription(`Opa ${member.user}, espero que você encontre novos amigos por aqui! 🤗`)
+        .setImage(`attachment://welcome.png`);
 
         const channel = member.guild.channels.cache.find(ch => ch.name === channel_welcome);
+
+        if (channel === null || channel === undefined) return;
 
         channel.send({ embeds: [welcomeEmbed], files: [file] });
 	},
